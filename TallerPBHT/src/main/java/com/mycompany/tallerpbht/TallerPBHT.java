69d0eab16522a69ade06fcd08482ec613a72531a
Isaac de Jesús Romero Martínez <isaac_7j@hotmail.com>
2024-11-22 22:24:43 -0600

    Creacion de la clase usuarios con sus atributos

---
+++ TallerPBHT/src/main/java/com/mycompany/tallerpbht/TallerPBHT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerpbht;
+
+/**
+ *
+ * @author IsaacdeJesús
+ */
+public class TallerPBHT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
